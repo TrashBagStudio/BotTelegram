f313f1cb3011d5c7905d32e1071372341612256f
TrashBagStudio <igorpaprov@gmail.com>
2026-04-22 16:50:53 +0300

    Fix support contact formatting in sup function

--- main.py
+++ main.py
@@ -284,7 +284,7 @@ async def card(callback: CallbackQuery):
 @dp.callback_query(F.data == "sup")
 async def sup(callback: CallbackQuery):
     await simple_page(callback, "images/main.png",
-                      "🛟 *Поддержка*\nЕсли у вас возникли проблемы или есть вопросы обратитесь в нашу поддержку:\n@your_support")
+                      "🛟 *Поддержка*\nЕсли у вас возникли проблемы или есть вопросы обратитесь в нашу поддержку:\nyour_support")
 
 
 @dp.callback_query(F.data == "info")
